f35b0983cbe80224aa0bddc251739919e0a565c3
vesper <future142025@gmail.com>
2026-07-22 00:22:23 +0330

    feat: add CPU information

--- system_info.py
+++ system_info.py
@@ -1,12 +1,21 @@
 import platform
 import socket
+import psutil
 
 
 computer_name = socket.gethostname()
 operating_system = platform.system()
 os_version = platform.release()
+cpu_cores = psutil.cpu_count(logical=False)
+cpu_threads = psutil.cpu_count(logical=True)
+cpu_usage = psutil.cpu_percent()
 
 print("\n=== System Information ===")
 print(f"Computer Name : {computer_name}")
 print(f"Operating System : {operating_system}")
 print(f"OS Version : {os_version}\n")
+
+print("\n=== CPU Information ===")
+print(f"Physical Cores : {cpu_cores}")
+print(f"Logical Cores : {cpu_threads}")
+print(f"CPU Usage : {cpu_usage}%")
